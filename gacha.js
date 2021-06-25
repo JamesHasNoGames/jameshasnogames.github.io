ab5ef860a2dd64790b6c6d514ef4df1bf9b7fd79
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-25 22:20:17 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1667,7 +1667,7 @@ function addChar(x, y, z){
 function editChar(x){
 	var parent = document.getElementById("container"+x);
 	var children = parent.getElementsByClassName("entry2hide");
-	demo.innerHTML = children[0]+children[1];
+	demo.innerHTML = children[0].innerHTML+children[1].innerHTML;
 	for(i = 0; i<=children.length; i++){
 			children[i].className = "entry2";
 			//demo.innerHTML = demo.innerHTML + "dddddddddddddddd" +children[i].innerHTML;
