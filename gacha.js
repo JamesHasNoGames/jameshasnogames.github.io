6cfcff05f1276e5446b35bc054c7cd67de955caf
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-26 18:12:27 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1694,8 +1694,8 @@ function max(x,y,z,n){
 			if (name[0] == chars[y][1]){
 				demo.innerHTML = "ok" + name[0];
 				x = x-10;
-				if(z++ == n){
-					
+				if(z == n-1){
+				z++;	
 				arr[i].innerHTML = '<div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="b1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="bempty2" id="b2"></div>';			
 
 				}else{
