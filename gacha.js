efa76d3e9e198db8d147b9fb5d8f2def8d48c519
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-26 19:06:29 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1666,6 +1666,7 @@ function addChar(x, y, z, n){
 	//}else{
 	//}
 	if(z > 0){
+
 	newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="bempty1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="bempty2" id="b2" onclick="max('+x+','+y+','+z+','+n+')"></div></div>';
     }else{
 	newcontent.innerHTML = '<div class="entry2hide"><div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="bempty1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="bempty2" id="b2" onclick="max('+x+','+y+','+z+','+n+')"></div></div>';
