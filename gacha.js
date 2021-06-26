090b2702b14327b2888162df03d36f3907829be7
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-26 18:09:24 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1694,12 +1694,16 @@ function max(x,y,z,n){
 			if (name[0] == chars[y][1]){
 				demo.innerHTML = "ok" + name[0];
 				x = x-10;
-				if(z==n){
-					z = 0;					
+				if(z++ == n){
+					z++;
+				arr[i].innerHTML = '<div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="b1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="bempty2" id="b2"></div>';			
+
 				}else{
 					z++;
+					arr[i].innerHTML = '<div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="b1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="b2" id="b2" onclick="max('+x+','+y+','+z+','+n+')"></div>';			
+
 				}				
-				arr[i].innerHTML = '<div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="b1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="b2" id="b2" onclick="max('+x+','+y+','+z+','+n+')"></div>';			
+				//arr[i].innerHTML = '<div class="name" style=" color: #ddd;">'+chars[y][1]+'</div><div class="b1" id="b1" onclick="min('+x+','+y+','+z+','+n+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+z+'</div><div class="b2" id="b2" onclick="max('+x+','+y+','+z+','+n+')"></div>';			
 				break;
 			}
 		}
