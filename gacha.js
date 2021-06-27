ad7f85ad5bd32c13c456cd210206649206769861
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-27 18:01:06 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1776,8 +1776,8 @@ function accept(x){
 		var nr = container.getElementsByClassName("nr5");
 		var jason = JSON.parse(localStorage.getItem("freeChars"))
 		for (var k = 0; k< jason.length; k++) {
-			//kaka.innerHTML = kaka.innerHTML + " " + jason[k];
-			freeChars.push([jason[k][0],jason[k][1],nr[k].textContent,jason[k][3]);	
+			kaka.innerHTML = kaka.innerHTML + " "+ nr.textContent;
+			//freeChars.push([jason[k][0],jason[k][1],nr[k].textContent,jason[k][3]);	
 		}
 		localStorage.removeItem("freeChars");
         localStorage.setItem("freeChars", JSON.stringify(freeChars));
