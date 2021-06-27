55b00b329a839fb21b695ac14511f385d06f65e4
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-27 17:51:06 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1779,7 +1779,7 @@ function accept(x){
 		}
 		localStorage.removeItem("freeChars");
         localStorage.setItem("freeChars", JSON.stringify(freeChars));*/
-		container.innerHTML = "hhh";
+		container.innerHTML = "";
 		parseFreeChar();
 	}
 }
