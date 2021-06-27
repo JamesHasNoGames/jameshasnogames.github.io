c8d0d17dac1fa18bd2efa64c629c49504e9c5b79
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-06-27 16:08:29 +0200

    h

--- gacha.js
+++ gacha.js
@@ -1697,19 +1697,19 @@ function editChar(x){
 	if(x==5){
 		var name;
 	var jason = JSON.parse(localStorage.getItem("freeChars"));
-	for (var k = 0; k< jason.length; k++) {  		
+	for (var k = 0; k< 2; k++) {  		
 			freeChars[k] = jason[k];
 			
 			if(freeChars[k][3]==freeChars[k][2]){
-				name = freeChars[k][1];
+				//name = freeChars[k][1];
 				children[k].innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+chars[freeChars[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeChars[k][0]+','+freeChars[k][1]+','+freeChars[k][2]+','+freeChars[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeChars[k][2]+'</div><div class="bempty2" id="b2"></div></div>';		
-			kaka.innerHTML = kaka.innerHTML +" one" + freeChars[k][1] + " " + k + children.length + "      ";
+			//kaka.innerHTML = kaka.innerHTML +" one" + freeChars[k][1] + " " + k + children.length + "      ";
 			}else if(freeChars[k][2]==0){
 				children[k].innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+chars[freeChars[k][1]][1]+'</div><div class="bempty1" id="b1"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeChars[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeChars[k][0]+','+freeChars[k][1]+','+freeChars[k][2]+','+freeChars[k][3]+')"></div></div>';		
-			kaka.innerHTML = kaka.innerHTML +" two" + freeChars[k][1] + " " + k;
+			//kaka.innerHTML = kaka.innerHTML +" two" + freeChars[k][1] + " " + k;
 			}else{
 				children[k].innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+chars[freeChars[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeChars[k][0]+','+freeChars[k][1]+','+freeChars[k][2]+','+freeChars[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeChars[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeChars[k][0]+','+freeChars[k][1]+','+freeChars[k][2]+','+freeChars[k][3]+')"></div></div>';			
-			kaka.innerHTML = kaka.innerHTML +" three" + k;
+			//kaka.innerHTML = kaka.innerHTML +" three" + k;
 			}
 
        }	
