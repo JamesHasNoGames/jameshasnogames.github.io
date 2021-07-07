98ceffb0de3d9d6a721b6fed62a36cb0cd889c85
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-07-07 05:54:42 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1940,7 +1940,7 @@ function max(x,y,z,n){
 			}
 		}else if (y>=1000){
 			y = y-1000;
-			demo.innerHTML = y;
+			demo.innerHTML = y + " " + x;
 			for (var i = 0; i<arr.length; i++){
 				name = arr[i].textContent.match(/[a-z A-Z]+|[0-9]+/g);
 				if (name[0] == weapons[y][1]){
