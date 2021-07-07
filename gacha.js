4100cfe658f06438e5ddfc4bcd0e386d93645d70
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-07-07 04:58:21 +0200

    Update gacha.js

--- gacha.js
+++ gacha.js
@@ -1889,11 +1889,11 @@ function editWeapons(x){
 			newcontent = document.createElement('div');  		
 			freeWeapons[k] = jason[k];
 			if(freeWeapons[k][3]==freeWeapons[k][2]){
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeWeapons[k][0]+','+freeWeapons[k][1]+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="bempty2" id="b2"></div></div>';		
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeWeapons[k][0]+','+Number(freeWeapons[k][1])+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="bempty2" id="b2"></div></div>';		
 			}else if(freeWeapons[k][2]==0){
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="bempty1" id="b1"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeWeapons[k][0]+','+freeWeapons[k][1]+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div></div>';		
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="bempty1" id="b1"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeWeapons[k][0]+','+Number(freeWeapons[k][1])+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div></div>';		
 			}else{
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeWeapons[k][0]+','+freeWeapons[k][1]+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeWeapons[k][0]+','+freeWeapons[k][1]+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div></div>';			
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[freeWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+freeWeapons[k][0]+','+Number(freeWeapons[k][1)+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+freeWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+freeWeapons[k][0]+','+Number(freeWeapons[k][1])+1000+','+freeWeapons[k][2]+','+freeWeapons[k][3]+')"></div></div>';			
 			}
 		container.appendChild(newcontent);
        }	
@@ -1903,11 +1903,11 @@ function editWeapons(x){
 			newcontent = document.createElement('div');  		
 			shopWeapons[k] = jason[k];
 			if(shopWeapons[k][3]==shopWeapons[k][2]){
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+shopWeapons[k][0]+','+shopWeapons[k][1]+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="bempty2" id="b2"></div></div>';		
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+shopWeapons[k][0]+','+Number(shopWeapons[k][1])+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="bempty2" id="b2"></div></div>';		
 			}else if(shopWeapons[k][2]==0){
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="bempty1" id="b1"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+shopWeapons[k][0]+','+shopWeapons[k][1]+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div></div>';		
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="bempty1" id="b1"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+shopWeapons[k][0]+','+Number(shopWeapons[k][1])+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div></div>';		
 			}else{
-				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+shopWeapons[k][0]+','+shopWeapons[k][1]+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+shopWeapons[k][0]+','+shopWeapons[k][1]+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div></div>';			
+				newcontent.innerHTML = '<div class="entry2"><div class="name" style=" color: #ddd;">'+weapons[shopWeapons[k][1]][1]+'</div><div class="b1" id="b1" onclick="min('+shopWeapons[k][0]+','+Number(shopWeapons[k][1])+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div><div class="nr5" id="nr5" style=" color: #ddd;">'+shopWeapons[k][2]+'</div><div class="b2" id="b2" onclick="max('+shopWeapons[k][0]+','+Number(shopWeapons[k][1])+1000+','+shopWeapons[k][2]+','+shopWeapons[k][3]+')"></div></div>';			
 			}
 		container.appendChild(newcontent);
        }	
