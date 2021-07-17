4bc7ff044f0de972145931615c55378312a1e866
JamesHasNoGames <62992062+JamesHasNoGames@users.noreply.github.com>
2021-07-17 08:17:07 +0200

    s

--- char.js
+++ char.js
@@ -124,7 +124,20 @@ var chars = [
 
 
 
-
+function createTable(elem, weapon, max){
+	var parent = document.getElementById(elem);
+	var newcontent = document.createElement('div');
+	var werte;
+	for (var i = 0; i<chars.length; i++){
+		if(chars[i][7] = elem & chars[i][6] = weapon){
+		werte.push(chars[i];	
+		}
+	}
+	
+	
+	newcontent.innerHTML = werte;
+	parent.appendChild(newcontent);
+}
 
 function tableContent(x){
     var a = document.getElementById("picture"+x);
